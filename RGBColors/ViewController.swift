978ba951ee898eeb8f22e740e4c99c05e69fa8a2
AlexeyNovoseltsev <113413099+AlexeyNovoseltsev@users.noreply.github.com>
2022-09-23 16:28:15 +0700

    Initial Commit

---
+++ RGBColors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RGBColors
+//
+//  Created by Alex Friend on 23.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
